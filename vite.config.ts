881dd064abcb2b7d510f4f61b785011642091993
jagathastroved <jagathratchagan@astroved.com>
2026-07-29 09:36:28 +0530

    legacy() plugin updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-
 import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      legacy({
+        targets: ['defaults', 'iOS >= 13', 'Safari >= 13'],
+      }),
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
